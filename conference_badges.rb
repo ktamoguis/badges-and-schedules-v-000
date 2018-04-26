7da6d654d83e2af4500798a6474c0b7ce73bf90c
Karl Amoguis <ktamoguis@gmail.com>
2018-04-26 06:00:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,6 @@
 # Write your code here.
-def badge_maker(name)
-  puts "Hello, my name is #{name}."
+def badge_maker(attendees)
+  attendees.each do |name|
+    puts "Hello, my name is #{name}."
+  end
 end
